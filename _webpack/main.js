614a3f7f84798dae084a57462a54a060512feaeb
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-05-03 22:10:32 -0400

    Import fontawesome 5 directly from node_modules

--- _webpack/main.js
+++ _webpack/main.js
@@ -1,4 +1,10 @@
 import Turbolinks from 'turbolinks';
+// Only Solid (fas) and Brands (fab) are free.
+// https://fontawesome.com/how-to-use/on-the-web/referencing-icons/basic-use
+import '@fortawesome/fontawesome-free/js/fontawesome.js';
+import '@fortawesome/fontawesome-free/js/brands.js';
+import '@fortawesome/fontawesome-free/js/solid.js';
+
 import initDisqus from './js/initDisqus';
 import initList from './js/initList';
 import initSvgDrawingAnimation from './js/initSvgDrawingAnimation';
