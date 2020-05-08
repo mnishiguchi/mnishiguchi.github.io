13b0b2721951b17a20a6818216e9233b3f33d1af
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-05-08 18:11:44 -0400

    Refactor bin/purgecss.js

--- bin/purgecss.js
+++ bin/purgecss.js
@@ -7,30 +7,69 @@ const PATHS = {
   content: glob.sync(`_site/**/*.html`),
   css: glob.sync(`_site/assets/main-bundle.css`),
 };
-console.log(PATHS);
 
-const cssBytesBeforeLookup = {};
-PATHS.css.forEach((cssFile) => {
-  try {
-    const data = fs.readFileSync(cssFile, 'utf8');
-    cssBytesBeforeLookup[cssFile] = data.length;
-  } catch (err) {
-    console.error(err);
+const printConfig = () =>
+  console.log(
+    'config',
+    `
+      content: ${PATHS.content.length.toString().padStart(3, ' ')} files
+      css:     ${PATHS.css.length.toString().padStart(3, ' ')} files`
+  );
+
+const printResult = ({ file, sizeBefore, sizeAfter, diff }) =>
+  console.log(
+    file,
+    `
+      before: ${sizeBefore.toString().padStart(8, ' ')}
+      after:  ${sizeAfter.toString().padStart(8, ' ')}
+      diff:   ${diff.toString().padStart(8, ' ')}`
+  );
+
+// Check CSS file sizes so that I can compare before and after later.
+const buildCssSizeLookup = () =>
+  PATHS.css.reduce((acc, cssFile) => {
+    try {
+      const data = fs.readFileSync(cssFile, 'utf8');
+      acc[cssFile] = data.length;
+      return acc;
+    } catch (err) {
+      console.error(err);
+    }
+  }, {});
+
+// For some reason, PurgeCSS's output option is not working so I need to update CSS file by myself.
+// https://github.com/FullHuman/purgecss-docs/issues/5#issuecomment-381926354
+const saveFile = (file, data) => {
+  fs.writeFile(file, data, (err) => {
+    console.log(err ? err : `Saved ${file}`);
+  });
+};
+
+(async function execute() {
+  const cssSizeLookup = buildCssSizeLookup();
+  if (!cssSizeLookup) {
+    console.error(`Error building CSS size lookup`);
+    return;
   }
-});
 
-// https://purgecss.com/api.html#usage
-(async function () {
-  const results = await new PurgeCSS().purge(PATHS);
-  results.forEach(({ css, file, rejected }, index) => {
-    const cssBytesBefore = cssBytesBeforeLookup[file];
-    const cssBytesAfter = css.length;
-    const diff = cssBytesAfter - cssBytesBefore;
-    console.log({ file, cssBytesBefore, cssBytesAfter, diff });
-    if (diff === 0) return;
+  printConfig();
 
-    // For some reason, output option is not working so I had to update CSS file.
-    // https://github.com/FullHuman/purgecss-docs/issues/5#issuecomment-381926354
-    fs.writeFile(file, css, (err) => console.log(err ? err : `Saved ${file}`));
+  // https://purgecss.com/api.html#usage
+  const results = await new PurgeCSS().purge({
+    content: PATHS.content,
+    css: PATHS.css,
+  });
+
+  results.forEach(({ css, file, rejected }) => {
+    if (rejected) {
+      console.error(`PurgeCSS rejected ${file}`);
+      return;
+    }
+    const sizeBefore = cssSizeLookup[file];
+    const sizeAfter = css.length;
+    const diff = sizeAfter - sizeBefore;
+    printResult({ file, sizeBefore, sizeAfter, diff });
+
+    if (diff < 0) saveFile(file, css);
   });
 })();
